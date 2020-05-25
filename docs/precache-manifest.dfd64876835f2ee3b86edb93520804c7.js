98cd59e5a7a524313c18c5ce51500af71317ae38
Martamp <motymont@gmail.com>
2020-05-25 20:53:29 +0200

    Create Docs folder

---
+++ docs/precache-manifest.dfd64876835f2ee3b86edb93520804c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b76f7b640abd6663eaa10fc0a6a121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c65d5c98a69c8524c8c",
+    "url": "/static/css/main.c9a71f0a.chunk.css"
+  },
+  {
+    "revision": "8f6a0d6382a7e91ebf4c",
+    "url": "/static/js/2.532cc9e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.532cc9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c65d5c98a69c8524c8c",
+    "url": "/static/js/main.d632151b.chunk.js"
+  },
+  {
+    "revision": "7f67fc81c0a881ee4509",
+    "url": "/static/js/runtime-main.85012d60.js"
+  },
+  {
+    "revision": "12db79b7a3a13bb009867830823fa48d",
+    "url": "/static/media/vision-logo.12db79b7.png"
+  }
+]);
